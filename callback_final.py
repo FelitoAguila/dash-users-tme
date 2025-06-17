8ef55a4c980e5f88af85e6f58f7c7a51fc32f1f1
FelitoAguila <feloaguila@gmail.com>
2025-06-17 10:22:36 -0300

    Update callback_final.py

--- callback_final.py
+++ callback_final.py
@@ -4,9 +4,10 @@ import dash_bootstrap_components as dbc
 from datetime import datetime
 from dotenv import load_dotenv
 import os
-from get_data import (get_daily_data, get_monthly_data, add_total_per_date, get_total_users_by_country,
+from get_data import (get_daily_data, get_monthly_data, add_total_per_date, get_total_free_users,
+                      get_heavy_free_users, aggregate_user_cycles, add_total_as_country, filter_user_cycles,
                       calculate_total_metrics, get_dau_mau_ratio_data)
-from charts import (active_users_chart, total_interactions_chart, heat_map_users_by_country,
+from charts import (active_users_chart, total_interactions_chart, heat_map_users_by_country, plot_user_histogram_faceted,
                     users_by_country, new_users_by_country, tree_map_users_by_country,
                     new_users_percentage_chart, interactions_percentage_chart, subs_by_country_chart,
                     dau_mau_ratio_chart, active_subscribed_users_chart, subscribed_users_percent_chart)
@@ -21,16 +22,27 @@ db_TME_charts = client['TranscribeMe-charts']
 collection_dau_by_country = db_TME_charts['dau-by-country']
 collection_mau_by_country = db_TME_charts['mau-by-country']
 collection_total_users_by_country = db_TME_charts['total-users-by-country']
+collection_free_cycles_by_country = db_TME_charts['free-cycles-by-country']
 
-# Cache para gráficos (datos que cambian según filtros)
-_charts_cache = {}
+
 
 # Calcular métricas una sola vez al importar el módulo
 TOTAL_METRICS = calculate_total_metrics(collection_dau_by_country, collection_mau_by_country)
 
-# Calcular total_users_df una sola vez al importar el módulo
-total_users_by_country = get_total_users_by_country(collection_total_users_by_country)
+# Cache para data de Free Users
+_free_users_cache = {}
 
+def get_free_users_chart_data (view):
+    cache_key = f"{view}"
+    if cache_key not in _free_users_cache:
+        if view == 'Total Free Users':
+            _free_users_cache[cache_key] = get_total_free_users(collection_free_cycles_by_country)
+        elif view == 'Heavy Free Users':
+            _free_users_cache[cache_key] = get_heavy_free_users(collection_free_cycles_by_country)
+    return _free_users_cache[cache_key]
+
+# Cache para gráficos (datos que cambian según filtros)
+_charts_cache = {}
 
 def get_chart_data(view, start_date, end_date):
     """Obtiene datos para gráficos con cache"""
@@ -100,17 +112,35 @@ def register_callbacks(app):
         """Renderiza el contenido según la pestaña seleccionada"""
         
         if active_tab == 'general':
+            usage_free_users = aggregate_user_cycles(collection_free_cycles_by_country)
+            usage_free_users = add_total_as_country(usage_free_users)
+            countries = list(usage_free_users['country'].unique())
+            countries = sorted([c for c in countries if c != 'Total']) + ['Total']
             return html.Div([
                 # Gráficos - Vista General
                 html.Div([
-                    html.Div([html.H3(f"{view} Active Users", style={'textAlign': 'center'}), dcc.Graph(id='total_active_users_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
-                    html.Div([html.H3(f"{view} New Users Percentage", style={'textAlign': 'center'}), dcc.Graph(id='new_users_percentage_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
-                    html.Div([html.H3(f"{view} Interactions", style={'textAlign': 'center'}), dcc.Graph(id='total_interactions_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
-                    html.Div([html.H3(f"{view} Audio and Text Percentage", style={'textAlign': 'center'}), dcc.Graph(id='audio_text_percentage_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
-                    html.Div([html.H3(f"{view} Active Subscribed Users", style={'textAlign': 'center'}), dcc.Graph(id='total_active_subscribed_users_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
-                    html.Div([html.H3(f"{view} Subscribed Users Percentage", style={'textAlign': 'center'}), dcc.Graph(id='subscribed_users_percent_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
-                    html.Div([html.H3("Total Free Users by Country", style={'textAlign': 'center'}), dcc.Graph(id='heat_map_users_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
-                    html.Div([html.H3("Country Share of Free Users", style={'textAlign': 'center'}), dcc.Graph(id='tree_map_users_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'})
+                    html.Div([html.H3(f"{view} Active Users", style={'textAlign': 'center'}), 
+                              dcc.Graph(id='total_active_users_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
+                    html.Div([html.H3(f"{view} New Users Percentage", style={'textAlign': 'center'}), 
+                              dcc.Graph(id='new_users_percentage_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
+                    html.Div([html.H3(f"{view} Interactions", style={'textAlign': 'center'}), 
+                              dcc.Graph(id='total_interactions_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
+                    html.Div([html.H3(f"{view} Audio and Text Percentage", style={'textAlign': 'center'}), 
+                              dcc.Graph(id='audio_text_percentage_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
+                    html.Div([html.H3(f"{view} Active Subscribed Users", style={'textAlign': 'center'}), 
+                              dcc.Graph(id='total_active_subscribed_users_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
+                    html.Div([html.H3(f"{view} Subscribed Users Percentage", style={'textAlign': 'center'}), 
+                              dcc.Graph(id='subscribed_users_percent_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
+                    # Free Users Charts
+                    html.Div([html.H3("Free Users by Country", style={'textAlign': 'center'}), 
+                              dcc.RadioItems(id = 'free_users_data', options = ['Total Free Users', 'Heavy Free Users'], value = 'Total Free Users',inline=True, labelStyle={'margin-right': '20px'}, style={'marginTop': '10px', 'textAlign': 'center'}), 
+                              dcc.Graph(id='heat_map_free_users_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
+                    html.Div([html.H3("Country Share of Free Users", style={'textAlign': 'center'}), 
+                              dcc.Graph(id='tree_map_free_users_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),
+                    html.Div([html.H3("Free Users Usage", style={'textAlign': 'center'}),
+                              dcc.RangeSlider(id = 'years', min=2023, max=2025, step =1, marks={2023: '2023', 2024:'2024', 2025:'2025'},value=[2023, 2025]), 
+                              dcc.Dropdown(id ='countries', options = countries, value = ['Argentina','Total'], multi=True),
+                              dcc.Graph(id='free_users_usage_fig')], style={'flex': '1', 'minWidth': '45%', 'margin': '10px', 'border': '1px solid #ddd', 'borderRadius': '5px', 'padding': '10px'}),                              
                 ], style={'display': 'flex', 'flexWrap': 'wrap', 'justifyContent': 'space-around'})
             ])
         elif active_tab == 'países':
@@ -148,16 +178,20 @@ def register_callbacks(app):
             Output('audio_text_percentage_fig', 'figure'),
             Output('total_active_subscribed_users_fig', 'figure'),
             Output('subscribed_users_percent_fig', 'figure'),
-            Output('heat_map_users_fig', 'figure'),
-            Output('tree_map_users_fig', 'figure')
+            Output('heat_map_free_users_fig', 'figure'),
+            Output('tree_map_free_users_fig', 'figure'), 
+            Output('free_users_usage_fig', 'figure')
         ],
         [
             Input('start_date_picker', 'date'), 
             Input('end_date_picker', 'date'),
-            Input('view_selector', 'value')
+            Input('view_selector', 'value'),
+            Input('free_users_data', 'value'),
+            Input('countries', 'value'),
+            Input('years', 'value')
         ]
     )
-    def update_general_charts(start_date, end_date, view):
+    def update_general_charts(start_date, end_date, view, free_users_data_selector,countries_list, year_range):
         """Actualiza gráficos generales según filtros seleccionados"""
         # Parsear fechas
         start = datetime.strptime(start_date[:10], '%Y-%m-%d')
@@ -180,13 +214,25 @@ def register_callbacks(app):
         audio_text_percentage_fig = interactions_percentage_chart(total_interactions_by_date, view)
         total_active_subscribed_users_fig = active_subscribed_users_chart(total_active_subscribed_users, view)
         subscribed_users_percent_fig = subscribed_users_percent_chart(total_active_subscribed_users, view)
-        heat_map_users_fig = heat_map_users_by_country(total_users_by_country)
-        tree_map_users_fig = tree_map_users_by_country(total_users_by_country)
         
+        # Total Free Users
+        free_users_data = get_free_users_chart_data(free_users_data_selector)
+
+        usage_free_users = aggregate_user_cycles(collection_free_cycles_by_country)
+        usage_free_users = add_total_as_country(usage_free_users)
+        filtered_df = filter_user_cycles(usage_free_users, countries_list, year_range)
+        
+        # Graficos 
+        heat_map_users_fig = heat_map_users_by_country(free_users_data, title = 'Heavy User condition: cycles_consumed >= max_cycles')
+        tree_map_users_fig = tree_map_users_by_country(free_users_data, title = 'Heavy User condition: cycles_consumed >= max_cycles')
+        free_users_usage_fig = plot_user_histogram_faceted (filtered_df)
+
         return (total_active_users_fig, new_users_percentage_fig, 
                 total_interactions_fig, audio_text_percentage_fig, 
                 total_active_subscribed_users_fig, subscribed_users_percent_fig,
-                heat_map_users_fig, tree_map_users_fig)
+                heat_map_users_fig, tree_map_users_fig,
+                free_users_usage_fig)
+
     
     # Callback para gráficos por país - SÍ cambian con filtros
     @app.callback(
